b4e03a7a48c7c0ee3a694988b08ee8383802a1b1
nimish-31 <95707677+nimish-31@users.noreply.github.com>
2024-11-06 23:33:33 +0530

    removed secret key

--- chaiheadq/settings.py
+++ chaiheadq/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+%_oya=y(m!)xbz0f3*0ly8w-$k)^+%vy2k1bxwkhba9ifl1c('
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
